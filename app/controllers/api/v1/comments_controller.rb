d44e29cae7f2b16c51c6eeefe92e1621d0a6d34c
Zhanzhi Lou <zhanzhilou@gmail.com>
2024-01-01 23:16:12 +0800

    added comment controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,53 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        comments=Comment.all
+        render json: comments
+    end
+
+    def create
+        comment=Comment.new(comment_params)
+
+        if comment.save
+            render json: comment, status: 200
+        else
+            render json:{
+                errors:comment.errors.messages
+            }
+        end
+    end
+
+    def update
+        comment = Comment.find_by(id: params[:id])
+        if comment.update(comment_params)
+            render json: comment
+        else
+            render json: { errors: comment.errors.messages }, status: 422
+        end
+    end
+
+    def destroy
+        comment = Comment.find_by(id: params[:id])
+        if comment.destroy
+            render json: "Comment Deleted"
+        else
+            render json: { errors: comment.errors.messages }, status: 422
+        end
+    end
+
+    def show
+        comment=Comment.find_by(id: params[:id])
+        if comment
+            render json: comment
+        else
+            render json:{
+                error: "Comment not found"
+            }
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:name,:content,:likes,:post_id)
+    end
 end
